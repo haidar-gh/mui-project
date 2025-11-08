8c251bcf4c14e7216bd6170d39e4a1a47f7906be
haidargharavi <haidar.gh.1534209@gmail.com>
2025-11-08 08:51:23 -0800

    fix data grid icon column

--- src/columns/columns.tsx
+++ src/columns/columns.tsx
@@ -81,7 +81,7 @@ const columnsPartnerProgram: GridColDef[] = [
         field: 'from', headerName: 'Amount', flex: 1, sortable: false,
         renderCell: (params) => (
             <Box display="flex" alignItems="center" gap={1}>
-                <img src='/public/images/USDT.png' style={{ width: 24, height: 24 }} />
+                <img src='/images/USDT.png' style={{ width: 24, height: 24 }} />
                 <span>{params.value}</span>
             </Box>
         ),
